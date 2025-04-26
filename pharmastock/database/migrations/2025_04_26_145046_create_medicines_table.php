e5ab8aa7bd1b9f25e5517369016cf628c3002f60
Matielo J Geronimo <matielojg@gmail.com>
2025-04-26 20:02:02 -0300

    Add Medicine model and migration for medicines table

---
+++ pharmastock/database/migrations/2025_04_26_145046_create_medicines_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medicines', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('manufacturer');
+            $table->integer('amount')->default(0);
+            $table->string('external_code')->nullable();
+            $table->unsignedBigInteger('user_id'); // Relaciona com o usuário que cadastrou
+            $table->timestamps();
+    
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medicines');
+    }
+};
