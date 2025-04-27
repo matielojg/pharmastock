df125888b0d984907193a448aded66d535100ac3
Matielo J Geronimo <matielojg@gmail.com>
2025-04-26 22:11:08 -0300

    feat: optimize search method in MedicineInfoController with caching

--- pharmastock/app/Http/Controllers/MedicineInfoController.php
+++ pharmastock/app/Http/Controllers/MedicineInfoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\MedicineInfo;
 use App\Http\Resources\MedicineInfoResource;
+use Illuminate\Support\Facades\Cache;
 
 class MedicineInfoController extends Controller
 {
@@ -13,30 +14,35 @@ class MedicineInfoController extends Controller
      */
     public function search(Request $request)
     {
-        $query = MedicineInfo::query();
+        $cacheKey = 'medicines_info_' . md5(json_encode($request->all()));
+        $cacheTtl = 600;
 
-        if ($request->filled('nome')) {
-            $query->where('nome_produto', 'LIKE', '%' . $request->nome . '%');
-        }
+        $results = Cache::remember($cacheKey, $cacheTtl, function () use ($request) {
 
-        if ($request->filled('principio')) {
-            $query->where('principio_ativo', 'LIKE', '%' . $request->principio . '%');
-        }
+            $query = MedicineInfo::query();
 
-        if ($request->filled('laboratorio')) {
-            $query->where('empresa_detentora_registro', 'LIKE', '%' . $request->laboratorio . '%');
-        }
+            if ($request->filled('nome')) {
+                $query->where('nome_produto', 'LIKE', '%' . $request->nome . '%');
+            }
 
-        if ($request->filled('classe')) {
-            $query->where('classe_terapeutica', 'LIKE', '%' . $request->classe . '%');
-        }
+            if ($request->filled('principio')) {
+                $query->where('principio_ativo', 'LIKE', '%' . $request->principio . '%');
+            }
 
-        if ($request->filled('registro')) {
-            $query->where('numero_registro_produto', 'LIKE', '%' . $request->registro . '%');
-        }
+            if ($request->filled('laboratorio')) {
+                $query->where('empresa_detentora_registro', 'LIKE', '%' . $request->laboratorio . '%');
+            }
 
-        $results = $query->paginate(10);
+            if ($request->filled('classe')) {
+                $query->where('classe_terapeutica', 'LIKE', '%' . $request->classe . '%');
+            }
 
+            if ($request->filled('registro')) {
+                $query->where('numero_registro_produto', 'LIKE', '%' . $request->registro . '%');
+            }
+
+            return $query->paginate(10);
+        });
         return MedicineInfoResource::collection($results);
     }
 }
